ab85a4a481c6089112c8b7c843ea92745d958929
Renee Nordholm <reneenordholm@gmail.com>
2019-05-31 17:03:47 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -5,6 +5,7 @@ class Genre
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def self.all
@@ -23,5 +24,9 @@ class Genre
     created_genre = Genre.new(genre)
     created_genre.save
     created_genre
+  end
+
+  def songs
+    @songs
   end
 end
