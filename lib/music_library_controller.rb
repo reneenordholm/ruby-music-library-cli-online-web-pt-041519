29f61f0a4e96bc8b6a50633899dd3b83dfd4948c
Renee Nordholm <reneenordholm@gmail.com>
2019-06-01 22:12:06 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,6 +6,8 @@ class MusicLibraryController
   end
 
   def call
+    input = ""
+      while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -15,5 +17,26 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    input = gets.chomp
+
+    case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+    end
+  end
+
+  def list_songs
+    Song.all.each.with_index(1) { |s, i| "#{i}. #{s}" }
   end
 end
